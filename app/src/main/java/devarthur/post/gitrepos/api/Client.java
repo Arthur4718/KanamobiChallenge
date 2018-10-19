efc91c5223b4036b59ff5823542b65c660f19389
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-19 00:15:03 -0300

    Client class
    Setting up retrofit builder.

--- app/src/main/java/devarthur/post/gitrepos/api/Client.java
+++ app/src/main/java/devarthur/post/gitrepos/api/Client.java
@@ -1,4 +1,24 @@
 package devarthur.post.gitrepos.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class Client {
+
+
+    public static final String GIT_URL = "https://api.github.com";
+    public static Retrofit retrofit;
+
+    public static Retrofit getClient(){
+        if(retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(GIT_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+        return retrofit;
+
+    }
+
 }
