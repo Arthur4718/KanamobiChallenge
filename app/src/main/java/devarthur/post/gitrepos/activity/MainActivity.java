d9413ef5ce9b066f1b96e00869522e1ef495c803
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-23 02:34:04 -0200

    Swipe on refresh.
    Added a new event listener to refresh the page when the user swipes it.

--- app/src/main/java/devarthur/post/gitrepos/activity/MainActivity.java
+++ app/src/main/java/devarthur/post/gitrepos/activity/MainActivity.java
@@ -1,10 +1,11 @@
 package devarthur.post.gitrepos.activity;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,23 +17,20 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 
-import org.json.JSONArray;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.ref.ReferenceQueue;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.HttpEntity;
-import cz.msebera.android.httpclient.entity.ByteArrayEntity;
-import cz.msebera.android.httpclient.entity.ContentType;
 import devarthur.post.gitrepos.R;
 import devarthur.post.gitrepos.adapter.RecyclerViewAdapter;
 import devarthur.post.gitrepos.model.GitrepoDataModel;
@@ -45,18 +43,13 @@ public class MainActivity extends AppCompatActivity
     private List<GitrepoDataModel> GitRepoList;
     private RecyclerView mRecyclerView;
     private RecyclerViewAdapter myRecyclerViewAdapter;
+    private SwipeRefreshLayout swipeContainer;
     private int datalenght;
+    private int page;
 
     //Constants
+    //TODO create a class to handle the GET request and remove the code from this activity.
     private static final String GITAPI_URL = "https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1";
-
-    //TODO check for the correct base URL in the doc, see the resources on trello for more info
-
-
-    //TODO create a login to populate recycler view with local data.
-
-
-    //TODO create a logic to populate the recycler view with data from GIT API
 
 
     @Override
@@ -77,6 +70,24 @@ public class MainActivity extends AppCompatActivity
 
         mRecyclerView = findViewById(R.id.myRecyclerView);
         GitRepoList = new ArrayList<>();
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setColorSchemeColors(getResources().getColor(R.color.colorAccent));
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                // progressBar = (ProgressBar) findViewById(R.id.progressBar);
+                Toast.makeText(getApplicationContext(), "Resolving new Repos",Toast.LENGTH_SHORT ).show();
+                //FOR DEBUG ONLY
+                //populateRecyclerView();
+                // progressBar.setVisibility(View.VISIBLE);
+                getDataFromGit();
+
+
+            }
+        });
     }
 
 
@@ -114,10 +125,12 @@ public class MainActivity extends AppCompatActivity
 
                         GitRepoList.add(gitItem);
                         feedRecyclerView(GitRepoList);
+
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
+                swipeContainer.setRefreshing(false);
 
             }
 
@@ -179,7 +192,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             //TODO Create new item navigations
-            getDataFromGit();
+
 
         } else if (id == R.id.nav_gallery) {
 
